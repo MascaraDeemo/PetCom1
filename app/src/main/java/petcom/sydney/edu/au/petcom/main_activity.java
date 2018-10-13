f928de7f33d09151d7ebe033ef9c9258388867e1
smartpetunia <lovepetunia@qq.com>
2018-10-13 17:17:09 +0800

    blabla

--- app/src/main/java/petcom/sydney/edu/au/petcom/main_activity.java
+++ app/src/main/java/petcom/sydney/edu/au/petcom/main_activity.java
@@ -80,17 +80,17 @@ public class main_activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_moment) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_personal) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_film) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_post) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_outdoor) {
 
         }
 
