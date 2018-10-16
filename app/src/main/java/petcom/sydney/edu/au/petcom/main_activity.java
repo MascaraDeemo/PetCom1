4d2e2207a2616a066a04fcbac0048312ded977b8
smartpetunia <lovepetunia@qq.com>
2018-10-16 14:21:38 +0800

    加入了个人主页

--- app/src/main/java/petcom/sydney/edu/au/petcom/main_activity.java
+++ app/src/main/java/petcom/sydney/edu/au/petcom/main_activity.java
@@ -140,7 +140,7 @@ public class main_activity extends AppCompatActivity
         } else if (id == R.id.nav_outdoor) {
 
         }else if (id== R.id.nav_editinfo){
-            Intent infoIntent = new Intent(this, AddToDatabase.class);
+            Intent infoIntent = new Intent(this, Personal_page.class);
             startActivity(infoIntent);
 
         }else if (id== R.id.nav_signout){
