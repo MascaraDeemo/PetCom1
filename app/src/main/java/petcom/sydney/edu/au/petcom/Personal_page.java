fde168022152dc6921211c23c66bf47f6edcef25
Sam Zhou <mascaradeemo@gmail.com>
2018-10-18 15:59:26 +1100

    Avatar and name on NavBar ok

--- app/src/main/java/petcom/sydney/edu/au/petcom/Personal_page.java
+++ app/src/main/java/petcom/sydney/edu/au/petcom/Personal_page.java
@@ -10,11 +10,13 @@ import android.widget.Button;
 import petcom.sydney.edu.au.petcom.UserProfiles.AddToDatabase;
 
 public class Personal_page extends AppCompatActivity {
+    Button edit_button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_page);
-        Button edit_button =(Button)findViewById(R.id.edit_btn);
+        edit_button =(Button)findViewById(R.id.edit_person_btn);
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
